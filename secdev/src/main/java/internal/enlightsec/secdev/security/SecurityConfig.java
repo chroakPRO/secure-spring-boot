49d5371d7c88beb059cc9d967311731aee67bc6a
coopersec <Christophereek97@gmail.com>
2022-06-02 07:21:32 +0200

    123

--- secdev/src/main/java/internal/enlightsec/secdev/security/SecurityConfig.java
+++ secdev/src/main/java/internal/enlightsec/secdev/security/SecurityConfig.java
@@ -1,43 +1,35 @@
 package internal.enlightsec.secdev.security;
 
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final LogoutHandler logoutHandler;
+	private final LogoutHandler logoutHandler;
 
-    public SecurityConfig(LogoutHandler logoutHandler) {
-        this.logoutHandler = logoutHandler;
-    }
+	public SecurityConfig(LogoutHandler logoutHandler) {
+		this.logoutHandler = logoutHandler;
+	}
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                // allow all users to access the home pages and the static images directory
-                .mvcMatchers("/", "/images/**").permitAll()
-                // all other requests must be authenticated
-                .anyRequest().authenticated()
-                .and().oauth2Login()
-                .and().logout()
-                // handle logout requests at /logout path
-                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                // customize logout handler to log out of Auth0
-                .addLogoutHandler(logoutHandler);
-                String[][] x = {{"O", "X", "X"},{"O", "X", "O"},{"X", "O", "X"}};
-    }
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
 
-// If using HS256, create a Bean to specify the HS256 should be used. By default, RS256 will be used.
-//    @Bean
-//    public JwtDecoderFactory<ClientRegistration> idTokenDecoderFactory() {
-//        OidcIdTokenDecoderFactory idTokenDecoderFactory = new OidcIdTokenDecoderFactory();
-//        idTokenDecoderFactory.setJwsAlgorithmResolver(clientRegistration -> MacAlgorithm.HS256);
-//        return idTokenDecoderFactory;
-//    }
+				.mvcMatchers("/", "/images/**").permitAll()
+
+				.anyRequest().authenticated()
+			.and().oauth2Login()
+			.and().logout()
+
+				.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+				.addLogoutHandler(logoutHandler);
+	}
+
 }
-
-
 
